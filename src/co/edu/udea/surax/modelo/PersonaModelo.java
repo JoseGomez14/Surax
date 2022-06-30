7e6579e8216830033390e3c317e78893863b9046
JoseG04 <jgcode04@gmail.com>
2022-06-30 18:14:35 -0500

    Create Project

---
+++ src/co/edu/udea/surax/modelo/PersonaModelo.java
@@ -0,0 +1,17 @@
+/*
+    Descripción de la clase
+*/
+
+package co.edu.udea.surax.modelo;
+
+/**
+ * 
+ * @author Jose D. Gómez M.
+ * @author Hector A. Güiza O
+ * @author Luis D. Martínez G.
+ * 
+ */
+
+public class PersonaModelo {
+    
+}
