58998e2d0cc78fd3a08cb980e502ac6f1664b8ce
JoseG04 <jgcode04@gmail.com>
2022-06-30 18:11:46 -0500

    Create Project

---
+++ src/co/edu/udea/surax/controlador/PersonaControlador.java
@@ -0,0 +1,17 @@
+/*
+    Descripción de la clase
+*/
+
+package co.edu.udea.surax.controlador;
+
+/**
+ * 
+ * @author Jose D. Gómez M.
+ * @author Hector A. Güiza O
+ * @author Luis D. Martínez G.
+ * 
+ */
+
+public class PersonaControlador {
+    
+}
