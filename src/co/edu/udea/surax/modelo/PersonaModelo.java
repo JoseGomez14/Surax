6594002155ad70b4a1ce34e8c3881884f028f540
Eljac <Eljac@Andres>
2022-07-04 18:55:04 -0500

    Persona y persona Natural

--- src/co/edu/udea/surax/modelo/PersonaModelo.java
+++ src/co/edu/udea/surax/modelo/PersonaModelo.java
@@ -1,33 +1,81 @@
 /*
     Descripción de la clase
-*/
-
+ */
 package co.edu.udea.surax.modelo;
 
+import java.util.ArrayList;
+
 /**
- * 
+ *
  * @author Jose D. Gómez M.
  * @author Hector A. Güiza O
  * @author Luis D. Martínez G.
- * 
+ *
  */
-
 public class PersonaModelo {
-    
-    private String name;
-    private Integer id;
-    private Integer age;
-    private Integer tel;
 
-    public PersonaModelo(String name, Integer id, Integer age, Integer tel) {
-        this.name = name;
+    private String nombre;
+    private int id;
+    private int tel;
+    // En 0 país, en 1 departamento, en 2 municipio, en 3 zona (Rural, Urbano), en 4 direccion, en 5 código postal 
+    private ArrayList<String> direccion;
+    private String correo;
+
+    public PersonaModelo() {
+
+    }
+
+    //Constructores
+    public PersonaModelo(String nombre, int id, int tel, ArrayList<String> direccion, String correo) {
+        this.nombre = nombre;
         this.id = id;
-        this.age = age;
+        this.tel = tel;
+        this.direccion = direccion;
+        this.correo = correo;
+    }
+
+    //Setters & Getters
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
         this.tel = tel;
     }
-    
-    public PersonaModelo(){
-        
+
+    public ArrayList<String> getDireccion() {
+        return direccion;
     }
 
+    public void setDireccion(ArrayList<String> direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    
+    
 }
