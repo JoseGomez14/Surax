f5f72a41192dc597f76fa64fdd5a2b571ff5fd0a
Eljac <Eljac@Andres>
2022-07-06 20:58:50 -0500

    Estudiante y persona Juridica

---
+++ src/co/edu/udea/surax/modelo/JuridicaModelo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.surax.modelo;
+
+/**
+ *
+ * @author Jose D. Gómez M.
+ * @author Hector A. Güiza O
+ * @author Luis D. Martínez G.
+ *
+ */
+public class JuridicaModelo extends PersonaModelo{
+    
+    //La razón social se guarda en la variable "nombre" de la clase padre
+    
+    private String actividadPrincipal;
+    private String sector;
+
+    public JuridicaModelo(String actividadPrincipal, String sector) {
+        this.actividadPrincipal = actividadPrincipal;
+        this.sector = sector;
+    }
+    
+    public JuridicaModelo(){
+        this.actividadPrincipal = null;
+        this.sector = null;
+    }
+    
+    //Setters & Getters
+
+    public String getActividadPrincipal() {
+        return actividadPrincipal;
+    }
+
+    public void setActividadPrincipal(String actividadPrincipal) {
+        this.actividadPrincipal = actividadPrincipal;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+    
+    
+    
+}
